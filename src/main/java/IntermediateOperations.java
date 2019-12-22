45953ba40acf6e155519a3e1e76d13944a7de261
Ankit Katiyar <ankitkatiyar67@gmail.com>
2019-12-22 13:21:34 +0530

    Stream example

---
+++ src/main/java/IntermediateOperations.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class IntermediateOperations {
+
+    public static void main(String[] args) {
+        List<ResturantMenu> resturantMenus = new ArrayList<>();
+        resturantMenus.add(new ResturantMenu("Paneer", 100));
+        resturantMenus.add(new ResturantMenu("Chicken", 300));
+        resturantMenus.add(new ResturantMenu("Potato", 500));
+        resturantMenus.add(new ResturantMenu("Cheese", 200));
+        resturantMenus.add(new ResturantMenu("Pizza", 600));
+
+        System.out.println("Thread: " + Thread.currentThread().getName());
+
+        Long time = System.nanoTime();
+        Stream<ResturantMenu> stream = resturantMenus.stream();
+        System.out.println("Dishes: " + streamFilter(stream));
+        System.out.println("Time Taken: " + (System.nanoTime() - time));
+        time = System.nanoTime();
+
+        System.out.println("Dishes: " + streamFilter(resturantMenus.parallelStream()));
+        System.out.println("Time Taken: " + (System.nanoTime() - time));
+
+        try {
+            System.out.println("Trying old stream again ###########");
+            System.out.println("Dishes: " + streamFilter(stream));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static List<String> streamFilter(Stream<ResturantMenu> stream) {
+        return stream.filter((item) -> {
+            System.out.println("Thread: " + Thread.currentThread().getName() + " Filter: " + item.getName() + " Calories:" + item.getCalories());
+            return item.getCalories() < 300;
+        }).map(item -> {
+            System.out.println("Thread: " + Thread.currentThread().getName() + " Mapping: " + item.getName());
+            return item.getName();
+        }).collect(Collectors.toList());
+    }
+
+
+}
